8f0319deb644db2792ad4acefbfdaab137ad16de
Cerberus <cerberus1220@gmail.com>
2014-12-14 17:06:54 +0800

    Fix pagination error

--- lib/filters/base.rb
+++ lib/filters/base.rb
@@ -5,14 +5,17 @@ module Pigeon
   
       def initialize(params = {})
         params.each do |attr, value|
-          self.public_send("#{attr}=", value)
+          begin
+            self.public_send("#{attr}=", value)
+          rescue NoMethodError
+          end
         end if params
       end
       
       def paginate(query, page, per_page = 40)
         current_page = page.to_i
         page_size = (query.count.to_f / per_page).ceil
-        [current_page, page_size,  query.offset(40 * current_page).limit(per_page)]
+        [current_page, page_size,  query.offset(per_page * current_page).limit(per_page)]
       end
     end
   end
